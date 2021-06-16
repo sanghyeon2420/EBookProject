1f98befe6f5ab3c9a2c74bfd9c4cca9da2534c8f
mrhi04-18 <mrhi04-18@DESKTOP-U25MTT5>
2021-06-16 15:48:48 +0900

    공지사항 수정 / notic 파일 생성 / 공지사항 접속시 views- notic - list 로 경로수정필요 / 현재경로 views -notic

--- EBookProject/src/main/java/com/example/EBookProject/model/dao/NoticDAO.java
+++ EBookProject/src/main/java/com/example/EBookProject/model/dao/NoticDAO.java
@@ -1,9 +1,85 @@
 package com.example.EBookProject.model.dao;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import com.example.EBookProject.model.dto.NoticDTO;
 
-public interface NoticDAO {
-	public List<NoticDTO> list();
+@Repository
+public class NoticDAO {
+	@Autowired
+	private SqlSessionTemplate mybatis;
+
+	@Inject
+	SqlSession sqlSession;
+	
+	public List<NoticDTO> list() {
+		return sqlSession.selectList("notic.list");
+	};
+	// 게시글 쓰기
+	public void createNotic(NoticDTO vo) {
+		mybatis.insert("NoticDAO.createBoard", vo);
+	}
+
+	// 게시글 수정
+	public void updateNotic(NoticDTO vo) {
+		mybatis.update("NoticDAO.updateBoard", vo);
+	}
+
+	// 게시글 삭제
+	public void deleteNotic(int notic_no) {
+		mybatis.update("NoticDAO.deleteBoard", notic_no);
+	}
+	
+	// 게시글 삭제 체크
+	public int checkNotic(int notic_no) throws Exception {
+		return mybatis.selectOne("NoticDAO.checkBoard", notic_no);
+	}
+
+	// 게시글 읽기
+	public NoticDTO readNotic(int notic_no) {
+		return (NoticDTO) mybatis.selectOne("NoticDAO.readBoard", notic_no);
+	}
+	
+	// 이전글
+	public NoticDTO previousB(int notic_no) {
+		return (NoticDTO) mybatis.selectOne("NoticDAO.previousB", notic_no);
+	}
+
+	// 다음글
+	public NoticDTO nextB(int notic_no) {
+		return (NoticDTO) mybatis.selectOne("NoticDAO.nextB", notic_no);
+	}
+	
+	// 게시물 목록
+	public List<NoticDTO> listAllNotic(String searchOption, String keyword, int start, int end) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("searchOption", searchOption);
+		map.put("keyword", keyword);
+		map.put("start", start);
+		map.put("end", end);
+		return mybatis.selectList("NoticDAO.listAllNotic", map);
+	}
+
+	// 조회수
+	public void notic_viewCnt(int notic_no) {
+		mybatis.update("NoticDAO.viewCnt", notic_no);
+	}
+	// 게시글 갯수
+		public int getCountNotic(String searchOption, String keyword) throws Exception {
+			Map<String, String> map = new HashMap<String, String>();
+			map.put("searchOption", searchOption);
+			map.put("keyword", keyword);
+			return mybatis.selectOne("NoticDAO.getCountNotic", map);
+		}
+	
+	
 }
--- EBookProject/src/main/java/com/example/EBookProject/model/service/impl/NoticServiceImpl.java
+++ EBookProject/src/main/java/com/example/EBookProject/model/service/impl/NoticServiceImpl.java
@@ -1,24 +1,164 @@
 package com.example.EBookProject.model.service.impl;
 
+import java.util.HashMap;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.example.EBookProject.Pager;
 import com.example.EBookProject.model.dao.impl.NoticDAOImpl;
 import com.example.EBookProject.model.dto.NoticDTO;
 import com.example.EBookProject.model.service.NoticService;
 
+@Controller
+@SessionAttributes("notic")
 @Service
-public class NoticServiceImpl implements NoticService {
+public class NoticServiceImpl{
 
 	@Inject
 	NoticDAOImpl dao;
-	
-	@Override
+
 	public List<NoticDTO> list() {
 		return dao.list();
 	}
+	
+	@Autowired
+	private NoticService noticService;
 
+	// 게시글 등록 뷰
+	@RequestMapping("/write")
+	public String write(NoticDTO vo) throws Exception {
+		return "notic/write";
+	}
+
+	// 게시글 등록
+	@RequestMapping(value = "/createNotic", method = RequestMethod.POST)
+	public String createNotic(NoticDTO vo) throws Exception {
+		noticService.createNotic(vo);
+		return "notic/list";
+	}
+
+	// 게시글 수정 뷰
+	@RequestMapping("/updateWrite")
+	public String updateWrite(NoticDTO vo) throws Exception {
+		return "notic/updateWrite";
+	}
+
+	// 게시글 수정
+	@RequestMapping(value = "/updateNotic", method = RequestMethod.POST)
+	public String updateBoard(NoticDTO vo) throws Exception {
+		if (vo.getNotic_title() != null) {
+			noticService.updateNotic(vo);
+			return "notic/view";
+		} else {
+			return "notic/write";
+		}
+		// return "redirect:/board";
+	}
+
+	// 게시글 삭제
+	@RequestMapping("/delete")
+	public String deleteNotic(int notic_no) throws Exception {
+		noticService.deleteNotic(notic_no); // 삭제 처리
+		return "redirect:/Notic"; // 목록으로 이동
+	}
+
+	// 게시글 목록 + 검색 + 페이징
+	@RequestMapping("/Notic")
+	public String listAllNotic(Model model, HttpSession session,
+			@RequestParam(defaultValue = "TITLE") String searchOption, 
+			@RequestParam(defaultValue = "") String keyword,
+			@RequestParam(defaultValue = "") String search, 
+			@RequestParam(defaultValue = "1") int curPage)
+			throws Exception {
+
+		// 게시글 갯수 계산
+		int count = noticService.getCountNotic(searchOption, keyword);
+
+		session.setAttribute("searchOption", searchOption);
+		session.setAttribute("keyword", keyword);
+		session.setAttribute("search", search);
+		session.setAttribute("curPage", curPage);
+
+		// 페이지 관련 설정
+		Pager pager = new Pager(count, curPage);
+		int start = pager.getPageBegin();
+		int end = pager.getPageEnd();
+
+		List<NoticDTO> list = noticService.listAllNotic(searchOption, keyword, start, end); // 게시글 목록
+
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("list", list); // map에 자료 저장
+		map.put("count", count);
+		map.put("pager", pager); // 페이지 네버게이션을 위한 변수
+		map.put("searchOption", searchOption);
+		map.put("keyword", keyword);
+		map.put("search", search);
+		model.addAttribute("map", map);
+
+		return "notic/list";
+	}
+
+	// 게시글 상세보기
+	@RequestMapping("/view")
+	public String readNotic(@RequestParam("notic_no") int notic_no, 
+							@RequestParam("notic_show") String notic_show, Model model, HttpSession session, 
+							@RequestParam(defaultValue = "TITLE") String searchOption,
+							@RequestParam(defaultValue = "") String keyword, 
+							@RequestParam(defaultValue = "") String search,
+							@RequestParam(defaultValue = "1") int curPage) throws Exception {
+
+		/* 게시글 예외처리 */
+		// 삭제된 게시글울 읽으려고 하는 경우
+		int checkNotic = noticService.checkNotic(notic_no);
+
+		if (checkNotic == 0 && notic_show.equals("N")) {
+			return "notic/checkDel";
+		}
+
+		// 존재하지 않는 게시글에 접근하려는 경우
+		else if (checkNotic == 0 && notic_show.equals("Y")) {
+			return "notic/checkNone";
+
+		} else if (checkNotic == 1 && notic_show.equals("N")) {
+			return "notic/checkNone";
+		}
+
+		noticService.notic_viewCnt(notic_no, session); // 조회수
+		model.addAttribute("notic", noticService.readNotic(notic_no)); // 게시글 읽기
+
+		// 세션 저장
+		searchOption = (String) session.getAttribute("searchOption");
+		session.setAttribute("searchOption", searchOption);
+
+		keyword = (String) session.getAttribute("keyword");
+		session.setAttribute("keyword", keyword);
+
+		search = (String) session.getAttribute("search");
+		session.setAttribute("search", search);
+
+//		curPage = (int) session.getAttribute("curPage");
+		curPage = (Integer) session.getAttribute("curPage");
+		session.setAttribute("curPage", curPage);
+
+		NoticDTO plist = noticService.previousB(notic_no);
+		NoticDTO nlist = noticService.nextB(notic_no);
+
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("previousB", plist);
+		map.put("nextB", nlist);
+		model.addAttribute("map", map);
+
+		return "notic/view";
+	}
 }
