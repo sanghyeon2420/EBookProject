3f991d0f9d7a18d6bc46c022c416fc190b1e5406
Kimsanghyeon <Kimsanghyeon@DESKTOP-SO1D47T>
2021-06-23 22:14:25 +0900

    업뎃

--- EBookProject/src/main/java/com/example/EBookProject/controller/MainController.java
+++ EBookProject/src/main/java/com/example/EBookProject/controller/MainController.java
@@ -9,24 +9,29 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.EBookProject.model.dto.BookDTO;
 import com.example.EBookProject.model.dto.BookcategoryDTO;
+import com.example.EBookProject.model.dto.NoticDTO;
 import com.example.EBookProject.model.dto.WriterDTO;
 import com.example.EBookProject.model.service.impl.BookServiceImpl;
+import com.example.EBookProject.model.service.impl.NoticServiceImpl;
 import com.example.EBookProject.model.service.impl.WriterServiceImpl;
 
 @Controller
 public class MainController {
 
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
-
+	
 	@Inject
 	BookServiceImpl bookservice;
 	
 	@Inject
 	WriterServiceImpl writerservice;
 	
+	@Inject
+	NoticServiceImpl noticservice;
 	
 	@RequestMapping("/")
 	public String index(HttpServletRequest request) {
@@ -40,7 +45,9 @@ public class MainController {
 		List<BookDTO> romancebook = bookservice.romancebook();  //로맨스 책 목록
 		
 		List<WriterDTO> writer_enter = writerservice.writer_enter();
-		
+
+		//List<NoticDTO> noticlist=noticservice.list();
+
 		// application 객체 사용 
 		// getServletContext() -> Returns: The ServletContext object for the web application
 		
@@ -49,6 +56,7 @@ public class MainController {
 		request.getSession().getServletContext().setAttribute("ranklist", ranklist);
 		request.getSession().getServletContext().setAttribute("romancebook", romancebook);
 		request.getSession().getServletContext().setAttribute("writer_enter", writer_enter);
+		//request.getSession().getServletContext().setAttribute("noticlist", noticlist);
 //		request.getSession().getServletContext().setAttribute("updatecontent", updatecontent);
 		//     /WEB-INF/views/index.jsp
 		return "index";
@@ -57,14 +65,13 @@ public class MainController {
 /*	@RequestMapping("notic")
 	public ModelAndView notic(ModelAndView mav) {
 		mav.setViewName("notic/list");
-//		mav.setViewName("notic"); // view 페이지  notic.jsp
-//		
-//		List<NoticDTO> list=noticservice.list();
-//		for (int i = 0; i < list.size(); i++) {
-//			System.out.println(list.get(i));
-//		}
-//		
-//		mav.addObject("list", list); // ModelAndView 객체에 list(공지사항 목록)을 저장
+		mav.setViewName("notic"); // view 페이지  notic.jsp
+		
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i));
+		}
+		
+		mav.addObject("list", list); // ModelAndView 객체에 list(공지사항 목록)을 저장
 		
 		return mav;
 	}*/
