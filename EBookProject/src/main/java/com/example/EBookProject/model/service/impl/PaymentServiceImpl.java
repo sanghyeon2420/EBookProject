3686e90e741a8deda9b0c6c8b278e0170db937ee
dksthdus410 <dksthdus0410@gmail.com>
2021-06-25 02:20:29 +0900

    쿠키 결제 하는 중

---
+++ EBookProject/src/main/java/com/example/EBookProject/model/service/impl/PaymentServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.EBookProject.model.service.impl;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import com.example.EBookProject.model.dao.impl.PaymentDAOImpl;
+import com.example.EBookProject.model.dto.PaymentDTO;
+import com.example.EBookProject.model.service.PaymentService;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+	@Inject
+	PaymentDAOImpl dao;
+
+	@Override
+	public PaymentDTO paymentbring(int user_no) {
+		return dao.paymentbring(user_no);
+	}
+
+
+}
