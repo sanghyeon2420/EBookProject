4b68521cc41d563b898d9289be63cf92ab0ad655
dksthdus410 <dksthdus0410@gmail.com>
2021-06-25 11:03:50 +0900

    a

--- EBookProject/src/main/java/com/example/EBookProject/controller/PaymentController.java
+++ EBookProject/src/main/java/com/example/EBookProject/controller/PaymentController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.EBookProject.model.dto.PaymentDTO;
@@ -17,15 +18,6 @@ public class PaymentController {
 	@Inject
 	PaymentServiceImpl service;
 	
-	@RequestMapping("paymentbring")
-	public ModelAndView paymentbring(HttpServletRequest request, ModelAndView mav, int idx) {
-		int user_no = idx;
-		PaymentDTO dto = service.paymentbring(user_no);
-		System.out.println(dto);
-		mav.addObject("paymentdto", dto);
-		mav.setViewName("payment/payment");
-		
-		return mav;
-	}
+	
 	
 }
--- EBookProject/src/main/java/com/example/EBookProject/model/dao/impl/PaymentDAOImpl.java
+++ EBookProject/src/main/java/com/example/EBookProject/model/dao/impl/PaymentDAOImpl.java
@@ -1,5 +1,8 @@
 package com.example.EBookProject.model.dao.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -17,6 +20,23 @@ public class PaymentDAOImpl implements PaymentDAO{
 	@Override
 	public PaymentDTO paymentbring(int user_no) {
 		return sqlSession.selectOne("payment.paymentbring", user_no);
+	}
+
+	@Override
+	public int contentBuy(int user_no, int contents_no) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("user_no",user_no);
+		map.put("contents_no", contents_no);
+		return sqlSession.update("payment.buy",map);
+		
+	}
+
+	@Override
+	public void contentResult(int user_no, int contents_no) {
+		Map<String, Object> map = new HashMap<>();
+		map.put("user_no",user_no);
+		map.put("contents_no", contents_no);
+		sqlSession.insert("payment.result",map);		
 	}
 
 	
